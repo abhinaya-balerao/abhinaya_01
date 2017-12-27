f10eb00b645bf95a0cd123a8fb1de1404e115539
Ram Sharma <ramsharma.india001@gmail.com>
2017-12-27 08:02:42 +0530

    Session on 27/12 7-8am IST

---
+++ java project_2/src/pkg/Hi.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		int x = 34;
+		String s = "asdf";
+		String s1 = new String("");
+	}
+
+}
