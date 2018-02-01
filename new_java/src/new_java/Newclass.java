92adacc736be0744021702222efc02b5ef552717
abhinaya_balerao <abhinaya1305@gmail.com>
2018-02-01 17:05:48 +0530

    Added new project

---
+++ new_java/src/new_java/Newclass.java
@@ -0,0 +1,14 @@
+package new_java;
+
+import org.testng.annotations.Test;
+
+public class Newclass {
+
+	@Test
+	private void syso() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+
+	}
+}
