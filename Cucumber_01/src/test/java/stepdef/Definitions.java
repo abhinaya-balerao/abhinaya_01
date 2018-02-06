b8145fd5c21f86323f1c1e28b2379eb3072a445b
Ram Sharma <ramsharma.india001@gmail.com>
2018-02-06 08:07:02 +0530

    session on 06/02

--- Cucumber_01/src/test/java/stepdef/Definitions.java
+++ Cucumber_01/src/test/java/stepdef/Definitions.java
@@ -1,49 +1,54 @@
 package stepdef;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Definitions {
-	@Given("^I want to write a step with precondition$")
-	public void i_want_to_write_a_step_with_precondition() throws Throwable {
-	    System.out.println("Abc");
+	public WebDriver driver;
+	
+	@Given("^browser has been launched$")
+	public void browser_has_been_launched() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "Browsers/chromedriver.exe");
+		driver = new ChromeDriver();
 	}
 
-	@Given("^some other precondition$")
-	public void some_other_precondition() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	
+	@When("^user opens url \"([^\"]*)\"$")
+	public void user_opens_url(String url) throws Throwable {
+		driver.get(url);
+	}
+	
+	@Then("^a page with title \"([^\"]*)\" should appear$")
+	public void a_page_with_title_should_appear(String title) throws Throwable {
+		 String pageTitle = driver.getTitle();
+		 Assert.assertEquals(pageTitle, title);
 	}
 
-	@When("^I complete action$")
-	public void i_complete_action() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@When("^user search for a text in google search box and press enter$")
+	public void user_search_for_a_text_in_google_search_box_and_press_enter() throws Throwable {
+		driver.findElement(By.name("q")).sendKeys("cat"+Keys.ENTER);
 	}
 
-	@When("^some other action$")
-	public void some_other_action() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Then("^atleast (\\d+) links related to that text should appear$")
+	public void atleast_links_related_to_that_text_should_appear(int linkSize) throws Throwable {
+		Assert.assertTrue(driver.findElements(By.xpath("//a")).size() > linkSize);
 	}
 
-	@When("^yet another action$")
-	public void yet_another_action() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@When("^user search for a text in amazon search box and press enter$")
+	public void user_search_for_a_text_in_amazon_search_box_and_press_enter() throws Throwable {
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("cat"+Keys.ENTER);
 	}
-
-	@Then("^I validate the outcomes$")
-	public void i_validate_the_outcomes() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^check more outcomes$")
-	public void check_more_outcomes() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	
+	@Then("^close the browser$")
+	public void close_the_browser() throws Throwable {
+	    driver.close();
 	}
 }
--- Cucumber_01/src/test/resources/basic.feature
+++ Cucumber_01/src/test/resources/basic.feature
@@ -1,14 +1,20 @@
 @tag
-Feature: Title of your feature
-  I want to use this template for my feature file
+Feature: Google
 
-  @tag1
-  Scenario: Title of your scenario
-    Given I want to write a step with precondition
-    And some other precondition
-    When I complete action
-    And some other action
-    And yet another action
-    Then I validate the outcomes
-    And check more outcomes
+  @google @all
+  Scenario: Search for an item in google
+    Given browser has been launched
+    When user opens url "https://www.google.co.in"
+    Then a page with title "Google" should appear
+    When user search for a text in google search box and press enter
+    Then atleast 5 links related to that text should appear
+    And close the browser
 
+	@amazon @all
+	Scenario: Search in amazon
+		Given browser has been launched
+    When user opens url "https://www.amazon.in/" 
+    Then a page with title "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in" should appear
+    When user search for a text in amazon search box and press enter
+    Then atleast 4 links related to that text should appear
+    And close the browser
